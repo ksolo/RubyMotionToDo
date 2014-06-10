f8c0b26af89201e69f9b76097047bdb293e225d2
Kevin Solorio <kevin@detone8.com>
2014-06-10 17:58:19 -0500

    visualize tableView

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,9 +2,12 @@ class ToDosController < UITableViewController
 
   def viewDidLoad
     super
+
+    @todos = [ToDo.new("Hate on Holacracy", nil), ToDo.new("Pack for NC", nil), ToDo.new("Something", nil)]
+
     self.title = "ToDos"
     self.tableView.registerClass(UITableViewCell, forCellReuseIdentifier: "ToDoCell")
-    @todos = [1, 2, 3]
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemAdd ,target: self, action: "add_item")
   end
 
   def tableView(tableView, numberOfRowsInSection: section)
@@ -16,8 +19,12 @@ class ToDosController < UITableViewController
     unless cell
       cell = UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: "ToDoCell")
     end
-    cell.textLabel.text = "Hello"
+    cell.textLabel.text = @todos[indexPath.row].name
     cell
+  end
+
+  def add_item
+    puts "I was pressed to add things!"
   end
 
 end
